b4bb470513906307869f80015c9131b426575fc7
dinhtuan242 <vudinhtuan242@gmail.com>
2024-07-09 23:48:02 +0700

    check api key

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { findById } = require("../services/apiKey.service")
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+
+const apiKey = async (req, res, next) => {
+    try {
+        const key = req.headers[HEADER.API_KEY]?.toString()
+        if (!key) {
+            return res.status(403).json({
+                message: 'Forbidden!'
+            })
+        }
+
+        // check obj key
+        const objKey = await findById(key)
+        if (!objKey) {
+            return res.status(403).json({
+                message: 'Forbidden!'
+            })
+        }
+        req.objKey = objKey
+        return next()
+    } catch (errors) {
+        console.log(errors);
+    }
+}
+
+module.exports = {
+    apiKey
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 'use strict'
 
 const express = require('express')
+const { apiKey } = require('../auth/checkAuth')
 const router = express.Router()
+
+// check api key
+router.use(apiKey)
+
+// check permission
 
 router.use('/v1/api', require('./access'))
 // router.use('/v1/api', require('./shop'))
